20874c124c4d009dea40c57228218449197c38c8
alanxia <alan.xia@nagarro.com>
2024-08-08 16:41:53 +0800

    updated

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,6 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Button, ConfigProvider } from 'antd';
 import { css } from '@emotion/css';
+
+import './css/intro.css';
 
 interface BtnProps {
     type?: string,
@@ -9,7 +11,21 @@ interface BtnProps {
     icon?: any
 }
 
+function setElementProps(className: string, {propName, value}: any) {
+  const items = document.querySelectorAll(className) as unknown as any[];
+  
+  items.forEach((p) => {
+    p.style.setProperty(propName, `${typeof p[value] === 'function' ?  p[value]() + 1 : value}`);
+  })
+}
+
+
 const Intro: React.FC<BtnProps> = ({icon}: BtnProps) => {
+
+  useEffect(() => {
+    setElementProps('.circle-self', {propName: '--l', value: 'getTotalLength'});
+  }, [])
+
   const { getPrefixCls } = useContext(ConfigProvider.ConfigContext);
   const rootPrefixCls = getPrefixCls();
   const linearGradientButton = css`
@@ -41,11 +57,19 @@ const Intro: React.FC<BtnProps> = ({icon}: BtnProps) => {
   }
 
   return (
-    <Button 
-        className={linearGradientButton} 
-        icon={icon} 
-        onClick={clickHandle}
-        type='primary' size='large' shape='circle' />
+    <>
+      <div className='btn-wrap'>
+        <Button 
+            className={`${linearGradientButton} btn-self`} 
+            icon={icon} 
+            onClick={clickHandle}
+            type='primary' size='large' shape='circle' />
+
+        <svg className='circle-svg' width="48" height="48" fill="none">
+          <circle className='circle-self' cx="24" cy="24" r="22" />
+        </svg>
+      </div>
+    </>
   );
 };
 
--- src/ts/ts-training/training02.ts
+++ src/ts/ts-training/training02.ts
@@ -2,7 +2,8 @@ type Props = {
     onClick: () => void,
     onDrag: () => void,
     news: string,
-    className: string
+    className: string,
+    class123: string,
 }
 
 // & 后面加的相当于是约束条件: {} 相当于通配符，模版字符串相当于正则表达式
@@ -12,3 +13,5 @@ type t1 = keyof Props & {} // => type t1 = "onClick" | "onDrag" | "news" | "clas
 type t2 = keyof Props & `on${string}` // => type t2 = "onClick" | "onDrag"
 
 type t3 = keyof Props & `${string}N${string}` // => type t3 = "className"
+
+type t4 = keyof Props & `${string}ss${number}` // => type t4 = "class123"
